8d5a30fd1c0f7d30fe018ed811925b4b59cf5e20
Torben Köhler <me@torben-koehler.de>
2024-07-07 12:01:16 +0200

    simplify StyledComponentsRegistry

--- src/lib/registry.tsx
+++ src/lib/registry.tsx
@@ -1,14 +1,11 @@
 'use client'
 
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
+import { ChildProps } from '@/lib/propTypes'
 
-export default function StyledComponentsRegistry({
-    children,
-}: {
-    children: React.ReactNode
-}) {
+export default function StyledComponentsRegistry({ children }: ChildProps) {
     // Only create stylesheet once with lazy initial state
     // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
     const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
