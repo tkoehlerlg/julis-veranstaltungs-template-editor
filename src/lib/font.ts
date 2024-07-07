8b1d65b8b0dc28dccb29282b3686ad9549a84dfc
Torben Köhler <me@torben-koehler.de>
2024-07-07 12:01:38 +0200

    abstract fonts

---
+++ src/lib/font.ts
@@ -0,0 +1,7 @@
+import { Inter, Montserrat } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+export const montserrat = Montserrat({
+    subsets: ['latin'],
+    variable: '--font-monserrat',
+})
