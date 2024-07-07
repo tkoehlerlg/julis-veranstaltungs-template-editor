3520eb1d15bc83b2982a919777069406cd86bf54
Torben Köhler <me@torben-koehler.de>
2024-07-07 12:01:53 +0200

    remove ThemeTemplate

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -1,4 +1,4 @@
-export function isColorLight(color: string, treshold = 0.8) {
+export function isColorLight(color: string, threshold = 0.8) {
     if (color.length == 7) {
         const rgb = [
             parseInt(color.substring(1, 3), 16),
@@ -9,16 +9,7 @@ export function isColorLight(color: string, treshold = 0.8) {
             (0.2126 * rgb[0]) / 255 +
             (0.7152 * rgb[1]) / 255 +
             (0.0722 * rgb[2]) / 255
-        return luminance > treshold
+        return luminance > threshold
     }
     return false
 }
-
-export const ThemeTemplate = {
-    blue: '#119EE5',
-    magenta: '#E5017C',
-    yellow: '#FEED00',
-    white: '#ffffff',
-    black: '#000000',
-    background: '#1E1F21',
-} as const
