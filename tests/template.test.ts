a9635e0caf4d7458d83bb4ce1f45c3485cf506ad
Torben Köhler <me@torben-koehler.de>
2024-08-16 20:49:43 +0200

    update tests

--- tests/template.test.ts
+++ tests/template.test.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test'
+import { env } from './env'
 
-// test('click on header', async ({ page }) => {
-//     await page.goto('/')
-//
-//     // Expect a title "to contain" a substring.
-//     const header = await page.getByText('Test')
-//     await expect(header).toHaveText('Test', { timeout: 5000 })
-// })
+test('click on header', async ({ page }) => {
+    await page.goto(env.BASE_URL)
+    const titleDiv = page.getByText('Titel')
+    await titleDiv.click()
+    await page.keyboard.insertText('Test')
+    await page.waitForTimeout(500)
+    await expect(titleDiv).toHaveText('Test', { timeout: 500 })
+})
