b33eb089d6d396a254b4dc0642b36a7eff07175e
Torben Köhler <me@torben-koehler.de>
2024-08-18 13:37:33 +0200

    add option to download images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,9 @@ import { Flex } from '@/components/common/Flex'
 import { css } from 'styled-components'
 import { useTheme } from '@/contexts/themeContext'
 import { CategoriesContainer } from '@/components/template/categoriesContainer'
-import { PopUp } from '@/components/common/PopUp'
+import { useRef } from 'react'
+import domToImage from 'dom-to-image'
+import { DownloadIcon } from 'lucide-react'
 
 export default function App() {
     return (
@@ -30,17 +32,41 @@ export default function App() {
 
 function TemplateEditor() {
     const theme = useTheme()
+    const divRef = useRef<HTMLDivElement>(null)
     const {
         isLoadingFromLocalStorage,
-        resetLocalStorage,
         selected,
         setSelected,
         templateBackgroundColor,
         cards,
         addCard,
+        titleCard,
         categories,
         editorSidebarRef,
     } = useTemplateEditorContext()
+
+    const saveAsImage = async () => {
+        if (divRef.current) {
+            try {
+                const dataUrl = await domToImage.toPng(divRef.current, {
+                    height: divRef.current.offsetHeight * 2.5,
+                    width: divRef.current.offsetWidth * 2.5,
+                    style: {
+                        transform: 'scale(2.5)',
+                        'transform-origin': 'top left',
+                    },
+                })
+
+                const link = document.createElement('a')
+                link.download = `${titleCard.title}.png`
+                link.href = dataUrl
+                link.click()
+            } catch (error) {
+                console.error('Error capturing div:', error)
+                alert('Failed to capture the div. Please try again.')
+            }
+        }
+    }
 
     return (
         <main>
@@ -48,6 +74,7 @@ function TemplateEditor() {
                 <ResizablePanel
                     defaultSize={75}
                     css={css`
+                        position: relative;
                         display: flex;
                         flex-direction: column;
                         height: 100dvh;
@@ -56,46 +83,47 @@ function TemplateEditor() {
                     `}
                     onClick={() => setSelected(undefined)}
                 >
-                    <Flex dir='row' align='center' gap={20}>
+                    <Flex
+                        dir='row'
+                        align='center'
+                        gap={20}
+                        styles={css`
+                            pointer-events: none;
+                        `}
+                    >
                         <h1
                             css={css`
                                 font-family: ${theme.font.montserrat};
                                 font-size: ${theme.fontSize.display6};
                                 font-weight: 900;
                                 color: ${theme.palette.template.magenta};
+                                pointer-events: auto;
                             `}
                         >
                             JuLis Veranstaltungs-Template-Editor
                         </h1>
-                        <button
-                            onClick={() => resetLocalStorage()}
-                            css={css`
-                                background-color: ${theme.palette.slate[100]};
-                                color: ${theme.palette.slate[800]};
-                                border: none;
-                                padding: 5px 10px;
-                                border-radius: 5px;
-                                cursor: pointer;
-                                font-size: ${theme.fontSize.extraSmall};
-                                font-weight: 500;
-                            `}
-                        >
-                            Reset
-                        </button>
                     </Flex>
-                    <div
-                        className={
-                            'flex h-full w-full flex-col items-center justify-center'
-                        }
+                    <Flex
+                        align='center'
+                        justify='center'
+                        styles={css`
+                            height: 100%;
+                            width: 100%;
+                            pointer-events: none;
+                        `}
                     >
                         {!isLoadingFromLocalStorage ? (
                             <TemplateBox
+                                ref={divRef}
                                 isSelected={selected?.type === 'background'}
                                 backgroundColor={templateBackgroundColor}
                                 onClick={() =>
                                     setSelected({ type: 'background' })
                                 }
-                                className={'scale-125'}
+                                css={css`
+                                    scale: 1.25;
+                                    pointer-events: auto;
+                                `}
                             >
                                 <TitleCard />
                                 <AddButton onClick={() => addCard(0)} />
@@ -122,7 +150,28 @@ function TemplateEditor() {
                         ) : (
                             <div>Lade von Lokalem Speicher...</div>
                         )}
-                    </div>
+                    </Flex>
+                    <button
+                        onClick={() => saveAsImage()}
+                        css={css`
+                            position: absolute;
+                            bottom: 20px;
+                            right: 20px;
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                            height: 40px;
+                            width: 40px;
+                            background-color: ${theme.palette.slate[100]};
+                            border: 1px solid ${theme.palette.slate[200]};
+                            border-radius: 50%;
+                        `}
+                    >
+                        <DownloadIcon
+                            size={20}
+                            color={theme.palette.slate[600]}
+                        />
+                    </button>
                 </ResizablePanel>
                 <ResizableHandle withHandle />
                 <ResizablePanel
