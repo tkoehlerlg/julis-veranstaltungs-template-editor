0cabb5690e027611573f3bb8ee2c56bd83ec5f79
Torben Köhler <me@torben-koehler.de>
2024-08-19 01:33:31 +0200

    Update env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,8 +1,9 @@
 import { cleanEnv, str, email, json } from 'envalid'
 
 export const env = cleanEnv(process.env, {
-    BASE_URL: str(),
+    NEXT_PUBLIC_SITE_URL: str(),
     NODE_ENV: str({
         choices: ['development', 'test', 'production', 'staging'],
+        default: 'production',
     }),
 })
