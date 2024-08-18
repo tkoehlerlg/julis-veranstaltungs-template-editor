1a97ca88b5869de75b6ba703486e8ee00d241455
Torben Köhler <me@torben-koehler.de>
2024-08-18 13:38:31 +0200

    add ref to TemplateBox (important for -2 commit)

--- src/components/template/templateBox.tsx
+++ src/components/template/templateBox.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react'
+import { ReactNode, forwardRef } from 'react'
 import { isColorLight } from '@/lib/color'
 import { cn } from '@/lib/utils'
 import { THEME } from '@/utils/theme'
@@ -11,34 +11,35 @@ type TemplateBoxProps = {
     children?: ReactNode | ReactNode[]
 }
 
-export function TemplateBox({
-    isSelected,
-    backgroundColor,
-    onClick,
-    className,
-    children,
-}: TemplateBoxProps) {
-    return (
-        <div
-            className={cn(
-                'box-border flex w-[480px] cursor-pointer flex-col gap-2 border-[2.5px] px-[27px] py-[16px]',
-                className
-            )}
-            style={{
-                backgroundColor: backgroundColor,
-                borderColor: isSelected
-                    ? isColorLight(backgroundColor)
-                        ? 'black'
-                        : THEME.palette.template.magenta
-                    : 'transparent',
-            }}
-            onClick={(e) => {
-                e.preventDefault()
-                e.stopPropagation()
-                onClick?.()
-            }}
-        >
-            {children}
-        </div>
-    )
-}
+const TemplateBox = forwardRef<HTMLDivElement, TemplateBoxProps>(
+    ({ isSelected, backgroundColor, onClick, className, children }, ref) => {
+        return (
+            <div
+                ref={ref}
+                className={cn(
+                    'box-border flex w-[480px] cursor-pointer flex-col gap-2 border-[2.5px] px-[27px] py-[16px]',
+                    className
+                )}
+                style={{
+                    backgroundColor: backgroundColor,
+                    borderColor: isSelected
+                        ? isColorLight(backgroundColor)
+                            ? 'black'
+                            : THEME.palette.template.magenta
+                        : 'transparent',
+                }}
+                onClick={(e) => {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    onClick?.()
+                }}
+            >
+                {children}
+            </div>
+        )
+    }
+)
+
+TemplateBox.displayName = 'TemplateBox'
+
+export { TemplateBox }
