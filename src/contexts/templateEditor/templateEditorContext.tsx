8eddeb297528993a9b3d31ed9fe2e0643d65498f
Torben Köhler <torben@sonic.tech>
2024-08-24 16:05:08 +0200

    Update templateEditorContext.tsx

--- src/contexts/templateEditor/templateEditorContext.tsx
+++ src/contexts/templateEditor/templateEditorContext.tsx
@@ -357,7 +357,7 @@ export function TemplateEditorContextProvider({ children }: ChildProps) {
             }
             setSelected({ type: 'card', uuid: newCard.uuid })
         },
-        [setCards]
+        [categories, setCards]
     )
 
     const updateCard = useCallback(
